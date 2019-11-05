9820fd76a9f4e094e633e1a23a13ebdfbfe0362b
Jens <jens.e.bouwens@gmail.com>
2019-11-05 10:26:17 +0100

    actual project

---
+++ OBJProgramming/OBJProgramming/Bag.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OBJProgramming
+{
+    class Bag
+    {
+        public int Colour;
+        public double Hight;
+        public double Width;
+        public double Deapth;
+        public bool Empty;
+    }
+}
---
+++ OBJProgramming/OBJProgramming/person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OBJProgramming
+{
+    class Person
+    {
+        public string name;
+        public int age;
+        public double hight;
+        public string gender;
+    }
+}
