8b438e582362f8934df0e416302ac18b2042b6fd
Jens <jens.e.bouwens@gmail.com>
2019-11-07 10:29:34 +0100

    added class person and created the variables.

---
+++ LibraryProject/LibraryProject/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryProject
+{
+    class Person
+    {
+        private string name;
+        private int age;
+        private string gender;
+        private int custumerid;
+        public bool hasbook;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        public string Gender
+        {
+            get { return gender; }
+            set { gender = value; }
+        }
+        public int CustumerID
+        {
+            get { return custumerid; }
+            set { custumerid = value; }
+        }
+    }
+}
