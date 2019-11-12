baa9595b65816799bc18c9dc786de4fed55fe0e5
Jens <jens.e.bouwens@gmail.com>
2019-11-12 10:17:03 +0100

    a bit of stuff added for now more is to come

---
+++ docs/js/javastuff.js
@@ -0,0 +1,14 @@
+function change(); {
+    document.getElementById("myButton1").value="That is the first one done.";
+}
+
+function createP(); {
+    var para = document.createElement("p");
+    var node = document.createTextNode("This solves the second asignment.");
+    para.appendChild(node);
+}
+
+function removeP(); {
+    var elmnt = document.getElementById("p1");
+    elmnt.remove();
+}
